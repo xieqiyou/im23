562c520cf6c652684da3b6a0566920d036eca2ec
xieqiyou <xieqiming1987@gmail.com>
2025-05-21 16:31:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-cd ..
-mv 10_fix_wifi_mac openwrt/target/linux/ramips/mt7621/base-files/etc/hotplug.d/ieee80211/10_fix_wifi_mac
-mv 02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+#cd ..
+#mv 10_fix_wifi_mac openwrt/target/linux/ramips/mt7621/base-files/etc/hotplug.d/ieee80211/10_fix_wifi_mac
+#mv 02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 # mv Makefile  openwrt/package/kernel/linux/Makefile
 # mv kernel-defaults.mk openwrt/include/kernel-defaults.mk
 # mv vermagic openwrt/vermagic
-cd openwrt
+#cd openwrt
