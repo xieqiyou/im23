7ef96a4d1c7f7e7fa118111498482a897f4ae132
xieqiyou <xieqiming1987@gmail.com>
2025-08-18 11:26:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/onchina/luci-app-nvr
+git clone https://github.com/onchina/luci-app-nvr package/openwrt-nvr
