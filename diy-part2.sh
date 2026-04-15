e0e1f74ccd7e3a83f2f1536a407d3569647f1e20
xieqiyou <xieqiming1987@gmail.com>
2026-04-15 10:11:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i '/grep .*mkhash md5 > .*\.vermagic/s/^/#/' include/kernel-defaults.mk
 sed -i '/^#.*grep .*mkhash md5 > .*\.vermagic/a\\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
 sed -i 's/^STAMP_BUILT:=.*_$(shell.*kconfig.pl.*)/# &/' package/kernel/linux/Makefile
