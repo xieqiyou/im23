394e3ed735284761e0eb53c1c3fa3fed48448bda
xieqiyou <xieqiming1987@gmail.com>
2026-06-12 09:06:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,14 @@ echo 'src-git luci_app_vnt https://github.com/lmq8267/luci-app-vnt' >>feeds.conf
 echo 'src-git AC https://github.com/kiss19776/openwrt-gecoosac' >>feeds.conf.default
 echo 'src-git Lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
 sed -i '2s|https://github.com/qosmio/sqm-scripts-nss.git|https://github.com/JuliusBairaktaris/sqm-scripts-nss.git|' feeds.conf.default
+# 替换 packages 行
+sed -i 's|src-git packages https://github.com/immortalwrt/packages.git|src-git packages https://github.com/immortalwrt/packages.git^d91fbd7451ffb1a9ec103df956844baddc18591c|' feeds.conf
+
+# 替换 luci 行
+sed -i 's|src-git luci https://github.com/immortalwrt/luci.git|src-git luci https://github.com/immortalwrt/luci.git^874556da6b3b9c5ab1f2c7d3231a1c8de7fb8b56|' feeds.conf
+
+# 替换 routing 行
+sed -i 's|src-git routing https://github.com/openwrt/routing.git|src-git routing https://github.com/openwrt/routing.git^776e7160636a7f1add27483ac926dadeb248bb65|' feeds.conf
+
+# 替换 telephony 行
+sed -i 's|src-git telephony https://github.com/openwrt/telephony.git|src-git telephony https://github.com/openwrt/telephony.git^4d8d33a023b24c52cd9443b9dc201fbdfe9c6aef|' feeds.conf
